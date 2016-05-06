273c8d939101c2d1703d1e5562f18ef4559cde9f
fernandoporazzi <fernandoporazzi@gmail.com>
2016-05-06 17:10:34 -0300

    Add initial data and get method

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const states = require('./states.json');
+
+var getStateByCode = (code) => {
+  let state = false;
+
+  for (var i = 0; i < states.length; i++) {
+    var self = states[i];
+
+    if (self.code === code) {
+      state = self;
+      break;
+    }
+  }
+
+  if (!state) {
+    return 'UF not found';
+  }
+
+  return state;
+}
+
+exports.get = (uf) => {
+  if (uf !== undefined) {
+    var uf = uf.trim();
+
+    if (uf === '') {
+      throw new Error('UF can not be empty');
+    }
+
+    if (uf !== '') {
+      return getStateByCode(uf);
+    }
+  }
+
+  if (uf === undefined) {
+    return states;
+  }
+};
